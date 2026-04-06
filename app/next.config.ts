a811c7f361775d55d6032f2cdd978e178ca25c7c
MatseMx <ma.chatzidakis@gmail.com>
2026-04-06 04:44:56 +0300

    deploy scripts

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   reactCompiler: true,
   reactStrictMode: true,
-  devIndicators: false
+  devIndicators: false,
 };
 
 export default nextConfig;
